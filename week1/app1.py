8501a09fed96daa09058f972caaaf988051e6c34
Vikram Raaja <126766536+VIKRAMRAAJA0407@users.noreply.github.com>
2024-06-09 16:28:44 +0530

    Update app1.py

--- week1/app1.py
+++ week1/app1.py
@@ -72,7 +72,7 @@ html_subtitle = """
 page_bg_img = '''
 <style>
 .stApp {
-    background-image: url("https://i0.wp.com/eos.org/wp-content/uploads/2020/07/data-streams-eos-august.png?fit=820%2C615&ssl=1");
+    background: grey;
     background-size: cover;
     background-repeat: no-repeat;
     background-attachment: fixed;
